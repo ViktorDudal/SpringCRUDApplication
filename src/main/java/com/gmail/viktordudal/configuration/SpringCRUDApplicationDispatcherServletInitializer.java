e385078c119ce78d07ea1789a626e0543d892afb
Viktor Dudal <viktordudal@gmail.com>
2023-01-31 21:45:37 +0200

    Initial commit

---
+++ src/main/java/com/gmail/viktordudal/configuration/SpringCRUDApplicationDispatcherServletInitializer.java
@@ -0,0 +1,21 @@
+package com.gmail.viktordudal.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringCRUDApplicationDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
