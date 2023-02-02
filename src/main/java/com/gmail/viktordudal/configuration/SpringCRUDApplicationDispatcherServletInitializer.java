d8f469a43ea50bc70efada632cf92996e2aba89d
Viktor Dudal <viktordudal@gmail.com>
2023-02-02 17:41:44 +0200

    Added PATCH and DELETE methods with DAO as ArrayList

--- src/main/java/com/gmail/viktordudal/configuration/SpringCRUDApplicationDispatcherServletInitializer.java
+++ src/main/java/com/gmail/viktordudal/configuration/SpringCRUDApplicationDispatcherServletInitializer.java
@@ -1,6 +1,10 @@
 package com.gmail.viktordudal.configuration;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class SpringCRUDApplicationDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -17,5 +21,17 @@ public class SpringCRUDApplicationDispatcherServletInitializer extends AbstractA
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        registerHiddenFieldFilter(servletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext context) {
+        context
+                .addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter())
+                .addMappingForUrlPatterns(null, true, "/*");
     }
 }
